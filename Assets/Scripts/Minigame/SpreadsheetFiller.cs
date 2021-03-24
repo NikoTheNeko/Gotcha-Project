16c303129b2d4e108667fcf71ee96f40cd0ecbe6
parmpreet001 <parmpreet1337@gmail.com>
2021-03-24 13:55:57 -0700

    Cleaned up code a bit
    Also made the max number of active cells a public variable, so it can be set to w/e

--- Assets/Scripts/Minigame/SpreadsheetFiller.cs
+++ Assets/Scripts/Minigame/SpreadsheetFiller.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class SpreadsheetFiller : MonoBehaviour
 {
-    public GameObject spreadSheetCell;
-    public GameObject spreadSheet;
-    public int spreadSheetHeight;
-    public int spreadSheetWidth;
-    public int rows;
-    public int cols;
-    public GameObject[,] cells;
+    public GameObject spreadSheetCell; //Reference to spreadSheetCell prefab. Used to create spreadsheet cells
+    public GameObject spreadSheet; //Reference to object cells are assigned to be the child of, this way there arent a million cells in the scene view
+    public int spreadSheetHeight; //Height of spreadsheet in pixels
+    public int spreadSheetWidth; //Width of spreadsheet in pixels
+    public int rows; //number of rows
+    public int cols; //number of collumns
+    public int maxActiveCells; //number of cells to be set to active when there are no active cells
+    private GameObject[,] cells; //Array of spread sheet cells
 
+    //Size of a spreadsheet cell in pixels
     private int cellHeight;
     private int cellWidth;
+    private int numberOfActiveCells;
 
-    private Vector2 mousePos;
+    private Vector2 rng; //Used to randomly select cells
+    private RaycastHit2D rayHit; //Used to detect what cell the mouse is hovering over
 
-    private Vector2 rng;
-
-    int numberOfActiveCells;
-
-    RaycastHit2D rayHit;
-    // Start is called before the first frame update
     void Start()
     {
+        //Calculate size of each cell
         cellHeight = spreadSheetHeight / rows;
         cellWidth = spreadSheetWidth / cols;
-        Debug.Log(spreadSheetCell.transform.localScale.x + "," + spreadSheetCell.transform.localScale.y);
+
         cells = new GameObject[rows, cols];
 
+        //These values are used to make sure spreadsheet is centered on x axis, and starts at the bottom of the screen
         int startX = -960;
         int startY = -540 + spreadSheetHeight - cellHeight;
         int centerXOffset = (1920 - (cellWidth * cols)) / 2;
 
+        //Populates cells array
         for(int i = 0; i < rows; i++)
         {
             for(int j = 0; j < cols; j++)
@@ -46,36 +47,40 @@ public class SpreadsheetFiller : MonoBehaviour
             }
         }
 
-        TurnOnCells(3);
+        TurnOnCells(maxActiveCells);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+            //If the mouse is hovering over a cell
             if(rayHit.collider != null && rayHit.collider.GetComponent<SpreadsheetCell>())
             {
+                //If the cell is active
                 if (rayHit.collider.GetComponent<SpreadsheetCell>().isActive())
                 {
                     rayHit.collider.GetComponent<SpreadsheetCell>().SetActive(false);
                     numberOfActiveCells -= 1;
                     if (numberOfActiveCells == 0)
-                        TurnOnCells(3);
-                }
-                    
-                Debug.Log("Clicked on cell " + rayHit.collider.GetComponent<SpreadsheetCell>().pos);
+                        TurnOnCells(maxActiveCells);
+                }  
+                //Debug.Log("Clicked on cell " + rayHit.collider.GetComponent<SpreadsheetCell>().pos);
             }
         }
     }
 
+    /**
+        Randomly selects n number of cells to be set to active
+    **/
     private void TurnOnCells(int n)
     {
         for(int i = 0; i < n; i++)
         {
             rng.x = Random.Range(0, cols - 1);
             rng.y = Random.Range(0, rows - 1);
+            //Ensures that the same cell won't be chosen twice
             while (cells[(int)rng.y, (int)rng.x].GetComponent<SpreadsheetCell>().isActive())
             {
                 rng.x = Random.Range(0, cols - 1);
