6131422b11ce8ce82bbfb54ed4f086ff310591a5
Nikolas Sanchez <yoshiniko1223@gmail.com>
2021-03-25 02:13:30 -0700

    Scene Transitions and Main Menu Created
    Scene Transitions are made so scenes flow between each other a lot more smoothly now. Ain't that a sight to see, or I guess not see right? They're set up with the scene change button and there's a prefab for it now hooray!
    
    Main Menu is set up and the points manager is also set up there too. You can click to go to the minigame but not back so that needs to be set up. You can also go to a rolling page but it's just the original back and forth to test.

--- Assets/Scripts/Scene Switching Scripts/SceneChange.cs
+++ Assets/Scripts/Scene Switching Scripts/SceneChange.cs
@@ -5,11 +5,25 @@ using UnityEngine.SceneManagement;
 
 
 public class SceneChange : MonoBehaviour{
+    public Animator Transition;
+    public float TransitionTime = 1f;
+
     /**
         This function will basically swap scenes
         add the scene you want to change to in the parameter
     **/
     public void SceneChangeTo(string SceneToChangeTo){
+        StartCoroutine(LoadLevel(SceneToChangeTo));
+    }
+
+    IEnumerator LoadLevel(string SceneToChangeTo){
+        //Play Anim
+        Transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(TransitionTime);
+
+        //Load Scene
         SceneManager.LoadScene(SceneToChangeTo, LoadSceneMode.Single);
     }
 
